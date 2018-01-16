180f5432dcac9037f58bfdd36e3e1a6d726744a6
antinka <antinka.katrinka@gmail.com>
2018-01-16 22:29:42 +0200

    update app

--- BlogUsingEF.DAL/Repositories/EFUnitOfWork.cs
+++ BlogUsingEF.DAL/Repositories/EFUnitOfWork.cs
@@ -89,24 +89,5 @@ namespace BlogUsingEF.DAL.Repositories
                 return anketRepository;
             }
         }
-        private bool disposed = false;
-
-        public virtual void Dispose(bool disposing)
-        {
-            if (!this.disposed)
-            {
-                if (disposing)
-                {
-                    db.Dispose();
-                }
-                this.disposed = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
     }
 }
